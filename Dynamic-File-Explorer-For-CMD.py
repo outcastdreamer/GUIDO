39b59b3dbcc7fa09b720a19d381b693415d9fea0
outcastdreamer <meetsaketsavarn@gmail.com>
2024-06-01 18:36:38 +0530

    Added background colour logic
    Signed-off-by: outcastdreamer <meetsaketsavarn@gmail.com>

--- Dynamic-File-Explorer-For-CMD.py
+++ Dynamic-File-Explorer-For-CMD.py
@@ -75,6 +75,9 @@ class FileExplorer:
         curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)  
         curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)  
         curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED) 
+        curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        self.stdscr.bkgd(' ', curses.color_pair(4))
+        
         curses.curs_set(0)
         self.stdscr.keypad(True)
         self.stdscr.nodelay(True)
